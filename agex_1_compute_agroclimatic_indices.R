153e7fa1a5c373c2e32b046a55b24daba61ac7fd
Achicanoy Estrella <h.a.achicanoy@cgiar.org>
2024-02-05 15:35:02 -0500

    indices for one growing season in two consecutive years

--- agex_1_compute_agroclimatic_indices.R
+++ agex_1_compute_agroclimatic_indices.R
@@ -8,10 +8,11 @@
 # R options and packages loading
 options(warn = -1, scipen = 999)
 suppressMessages(if(!require(pacman)){install.packages('pacman')}else{library(pacman)})
-suppressMessages(pacman::p_load(terra))
+suppressMessages(pacman::p_load(terra,lubridate))
 
 # Source agro-climatic indices functions
 source('https://raw.githubusercontent.com/haachicanoy/agroclimExtremes/main/agex_0_indices.R')
+grep2 <- Vectorize(FUN = grep, vectorize.args = 'pattern')
 
 # Directories
 root <- '//CATALOGUE/WFP_ClimateRiskPr1'
@@ -23,8 +24,8 @@ prc <- list.files(path = paste0(root,'/1.Data/AgERA5/precipitation_flux'), patte
 # One season
 # ------------------------------------------ #
 # One-year
-s1_ini <- terra::rast(paste0(root,'/agroclimExtremes/data/results/one_s1_ini_10km.tif'))
-s1_end <- terra::rast(paste0(root,'/agroclimExtremes/data/results/one_s1_end_10km.tif'))
+s1_ini <- terra::rast(paste0(root,'/agroclimExtremes/agex_raw_data/agex_one_s1_ini_10km_croplands.tif'))
+s1_end <- terra::rast(paste0(root,'/agroclimExtremes/agex_raw_data/agex_one_s1_end_10km_croplands.tif'))
 s1_dff <- s1_end - s1_ini
 s1_dff[s1_end < s1_ini] <- NA
 s1_ini[is.na(s1_dff)] <- NA
@@ -37,7 +38,7 @@ lapply(X = yrs, FUN = function(yr){
   rnf <- terra::rast(prc_flt); gc(T)
   rsl <- terra::rapp(x = rnf, first = s1_ini, last = s1_end, fun = p95); gc(T)
   names(rsl) <- c('p95','day')
-  terra::writeRaster(x = rsl, filename = paste0(root,'/agroclimExtremes/indices/p95/one_s1_p95_',yr,'.tif'), overwrite = T); gc(T)
+  terra::writeRaster(x = rsl, filename = paste0(root,'/agroclimExtremes/agex_indices/agex_p95/one_s1_p95_',yr,'.tif'), overwrite = T); gc(T)
   return('Done.\n')
 })
 
@@ -47,22 +48,49 @@ lapply(X = yrs, FUN = function(yr){
   rnf <- terra::rast(prc_flt); gc(T)
   rsl <- terra::rapp(x = rnf, first = s1_ini, last = s1_end, fun = cdd); gc(T)
   names(rsl) <- paste0('cdd_',yr)
-  terra::writeRaster(x = rsl, filename = paste0(root,'/agroclimExtremes/indices/cdd/one_s1_cdd_',yr,'.tif'), overwrite = T); gc(T)
+  terra::writeRaster(x = rsl, filename = paste0(root,'/agroclimExtremes/agex_indices/agex_cdd/one_s1_y1_cdd_',yr,'.tif'), overwrite = T); gc(T)
   return('Done.\n')
 })
 
-p95_idx <- index |> purrr::map(1) |> terra::rast()
-p95_whn <- index |> purrr::map(2) |> terra::rast()
+# Two-years
+s1_ini <- terra::rast(paste0(root,'/agroclimExtremes/agex_raw_data/agex_one_s1_ini_10km_croplands.tif'))
+s1_end <- terra::rast(paste0(root,'/agroclimExtremes/agex_raw_data/agex_one_s1_end_10km_croplands.tif'))
+s1_dff <- s1_end - s1_ini
+s1_dff[s1_end > s1_ini] <- NA
+s1_ini[is.na(s1_dff)] <- NA
+s1_end[is.na(s1_dff)] <- NA
 
-# Two-years (TO DO)
-yrs <- 1981:2023
-p95_index <- lapply(X = yrs, FUN = function(yr){
+# Percentile 95th of daily precipitation
+yrs <- 1979:2023
+lapply(X = yrs, FUN = function(yr){
   # Precipitation files filtered
-  prc_flt <- prc[grep(pattern = c(yr,yr+1), x = prc)] # To implement
-  rnf <- terra::rast(prc_flt)
-  rsl <- terra::rapp(x = rnf, first = s1_ini_5km, last = s1_end_5km, fun = p95)
-  terra::writeRaster(x = rsl, filename = paste0(root,'/yr_',yr,'_world.tif'))
-  return('Done.\n')
+  if(yr < 2023){
+    prc_flt <- prc[unlist(grep2(pattern = c(paste0('_',yr,'[0-9][0-9][0-9][0-9]_'),paste0('_',yr+1,'[0-9][0-9][0-9][0-9]_')), x = prc))] # To implement
+    rnf <- terra::rast(prc_flt)
+    dys <- ifelse(lubridate::leap_year(yr),366,365)
+    rsl <- terra::rapp(x = rnf, first = s1_ini, last = s1_end+dys, fun = p95)
+    terra::writeRaster(x = rsl, filename = paste0(root,'/agroclimExtremes/agex_indices/agex_p95/one_s1_y2_p95_',yr,'.tif'), overwrite = T); gc(T)
+    return('Done.\n')
+  } else {
+    break
+    return('Done.\n')
+  }
+})
+
+# Maximum number of consecutive dry days
+lapply(X = yrs, FUN = function(yr){
+  # Precipitation files filtered
+  if(yr < 2023){
+    prc_flt <- prc[unlist(grep2(pattern = c(paste0('_',yr,'[0-9][0-9][0-9][0-9]_'),paste0('_',yr+1,'[0-9][0-9][0-9][0-9]_')), x = prc))] # To implement
+    rnf <- terra::rast(prc_flt)
+    dys <- ifelse(lubridate::leap_year(yr),366,365)
+    rsl <- terra::rapp(x = rnf, first = s1_ini, last = s1_end+dys, fun = cdd)
+    terra::writeRaster(x = rsl, filename = paste0(root,'/agroclimExtremes/agex_indices/agex_cdd/one_s1_y2_cdd_',yr,'.tif'), overwrite = T); gc(T)
+    return('Done.\n')
+  } else {
+    break
+    return('Done.\n')
+  }
 })
 
 # ------------------------------------------ #
