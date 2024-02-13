e30751f2a17091111a63f4b1197f0eff58d97b49
Achicanoy Estrella <h.a.achicanoy@cgiar.org>
2024-02-13 15:07:13 -0500

    reducing amount of RAM memory used

--- agex_02_compute_fmadogram_clusters.R
+++ agex_02_compute_fmadogram_clusters.R
@@ -60,7 +60,7 @@ cdd_roi_fnl <- terra::app(x = cdd_roi,
                               # Mann-Kendall test for evaluating trend significance
                               trd <- Kendall::MannKendall(x_scl)$sl
                               cnd <- ifelse(trd <= 0.05, T, F)
-                              if(cnd){ # Remove trend
+                              if(cnd){ # Remove trend by fitting loess regression
                                 dfm <- data.frame(x = yrs, y = x_scl)
                                 fit <- loess(y ~ x, data = dfm)
                                 x_ntrd <- dfm$y - fit$fitted
@@ -72,7 +72,7 @@ cdd_roi_fnl <- terra::app(x = cdd_roi,
                             }
                             return(x_ntrd)
                           })
-names(cdd_roi_fnl) <- paste0('Y',yrs)
+names(cdd_roi_fnl) <- paste0('Y',yrs); gc(T)
 
 ## Transform into a data.frame
 cdd_roi_ntrd <- terra::as.data.frame(x = cdd_roi_fnl, xy = T, cell = T, na.rm = T)
@@ -96,59 +96,43 @@ get_fmado_dist <- function(x){
   DD_fmado <- parallelDist::parDist(x = t(V), method = 'manhattan', diag = T, upper = T)/(2*Tnb)
   return(DD_fmado)
 }
+# Trim distribution of F-madogram distances greater than 1/6
+cap_fmado_dist <- function(DD_fmado){
+  DD_fmado_adj <- pmin(DD_fmado, rep(1/6, length(DD_fmado)))
+  if(any(DD_fmado_adj > 1/6, na.rm = TRUE) == 1)
+    stop("ERROR: cap_fmado_dist didn't work")
+  return(DD_fmado_adj)
+}
 
 ## Obtain distance matrices
-geogr <- as.matrix(scale(cdd_roi_ntrd[,c('x','y')]))
-geogr_dist <- parallelDist::parDist(x = geogr, # Geographical distance
-                                    method = 'euclidean',
-                                    diag = T, upper = T)
-fmado_dist  <- get_fmado_dist(x)       # F-madogram distances matrix
-fmado_dist2 <- fmado_dist + geogr_dist # F-madogram + geography
-fmado_dist2 <- .6 * fmado_dist + .4 * geogr_dist # F-madogram + geography
+fmado_dist <- get_fmado_dist(x); gc(T) # F-madogram distances
+fmado_dist <- cap_fmado_dist(fmado_dist); gc(T) # Truncated F-madogram distances
 eucld_dist  <- parallelDist::parDist(x = t(x), # Euclidean distances matrix
                                      method = 'euclidean',
-                                     diag = T, upper = T)
-eucld_dist2 <- eucld_dist + geogr_dist # Euclidean + geography
+                                     diag = T, upper = T); gc(T)
 
 # Hierarchical clustering for distance matrices
-# fmado_hcl <- hclust(fmado_dist, method = 'complete') # F-madogram
-# eucld_hcl <- hclust(eucld_dist, method = 'complete') # Euclidean
-fmado_hdb <- dbscan::hdbscan(x = fmado_dist, minPts = 100)
-fmado_hcl <- fastcluster::hclust(fmado_dist, method = 'complete') # F-madogram
-# single, complete, average, mcquitty, ward.D, ward.D2, centroid, median
-fmado_hcl_sng <- fastcluster::hclust(fmado_dist, method = 'single') # F-madogram
-fmado_hcl_cmp <- fastcluster::hclust(fmado_dist, method = 'complete') # F-madogram
-fmado_hcl_avg <- fastcluster::hclust(fmado_dist, method = 'average') # F-madogram
-fmado_hcl_mcq <- fastcluster::hclust(fmado_dist, method = 'mcquitty') # F-madogram
-fmado_hcl_wrd <- fastcluster::hclust(fmado_dist, method = 'ward.D') # F-madogram
-fmado_hcl_wd2 <- fastcluster::hclust(fmado_dist, method = 'ward.D2') # F-madogram
-fmado_hcl_cnt <- fastcluster::hclust(fmado_dist, method = 'centroid') # F-madogram
-fmado_hcl_mdn <- fastcluster::hclust(fmado_dist, method = 'median') # F-madogram
-eucld_hcl <- fastcluster::hclust(eucld_dist, method = 'complete') # Euclidean
-geogr_hcl <- fastcluster::hclust(geogr_dist, method = 'complete') # F-madogram
+fmado_hcl <- fastcluster::hclust(fmado_dist, method = 'ward.D2') # F-madogram
+eucld_hcl <- fastcluster::hclust(eucld_dist, method = 'ward.D2') # Euclidean
+# fmado_hdb <- dbscan::hdbscan(x = fmado_dist, minPts = 100) # Hierarchical DBSCAN
+# geogr_hcl <- fastcluster::hclust(geogr_dist, method = 'complete') # F-madogram
 
 # Query data.frame
 qry <- cdd_roi_ntrd[,c('cell','x','y')]
 
-qry$fmado_cluster <- cutree(tree = fmado_hcl, k = 30)
-qry$fmado_cluster_sng <- cutree(tree = fmado_hcl_sng, k = 30)
-qry$fmado_cluster_cmp <- cutree(tree = fmado_hcl_cmp, k = 30)
-qry$fmado_cluster_avg <- cutree(tree = fmado_hcl_avg, k = 30)
-qry$fmado_cluster_mcq <- cutree(tree = fmado_hcl_mcq, k = 30)
-qry$fmado_cluster_wrd <- cutree(tree = fmado_hcl_wrd, k = 30)
-qry$fmado_cluster_wd2 <- cutree(tree = fmado_hcl_wd2, k = 30)
-qry$fmado_cluster_cnt <- cutree(tree = fmado_hcl_cnt, k = 30)
-qry$fmado_cluster_mdn <- cutree(tree = fmado_hcl_mdn, k = 30)
-qry$eucld_cluster <- cutree(tree = eucld_hcl, k = 30)
+# How to determine number of clusters?
 
-saveRDS(object = qry, file = paste0(root,'/agroclimExtremes/results/cluster_info.rds'))
+qry$fmado_cluster <- cutree(tree = fmado_hcl, k = 10)
+qry$eucld_cluster <- cutree(tree = eucld_hcl, k = 10)
 
-res <- NbClust::NbClust(diss     = fmado_dist,
-                        distance = NULL,
-                        min.nc   = 2,
-                        max.nc   = 140,
-                        method   = 'ward.D2',
-                        index    = 'frey')
+# saveRDS(object = qry, file = paste0(root,'/agroclimExtremes/results/cluster_info.rds'))
+
+# res <- NbClust::NbClust(diss     = fmado_dist,
+#                         distance = NULL,
+#                         min.nc   = 2,
+#                         max.nc   = 140,
+#                         method   = 'ward.D2',
+#                         index    = 'frey')
 
 # cut_heights = data.frame(h = full_tree$height,
 #                          k = (nrow(cdd_roi_dfm)-1):1)
@@ -158,31 +142,23 @@ tmp <- cdd_roi_fnl[[1]]
 terra::values(tmp) <- NA
 
 fmado_r <- tmp; fmado_r[qry$cell] <- qry$fmado_cluster; plot(fmado_r)
-fmado_r_sng <- tmp; fmado_r_sng[qry$cell] <- qry$fmado_cluster_sng
-fmado_r_cmp <- tmp; fmado_r_cmp[qry$cell] <- qry$fmado_cluster_cmp
-fmado_r_avg <- tmp; fmado_r_avg[qry$cell] <- qry$fmado_cluster_avg
-fmado_r_mcq <- tmp; fmado_r_mcq[qry$cell] <- qry$fmado_cluster_mcq
-fmado_r_wrd <- tmp; fmado_r_wrd[qry$cell] <- qry$fmado_cluster_wrd
-fmado_r_wd2 <- tmp; fmado_r_wd2[qry$cell] <- qry$fmado_cluster_wd2
-fmado_r_cnt <- tmp; fmado_r_cnt[qry$cell] <- qry$fmado_cluster_cnt
-fmado_r_mdn <- tmp; fmado_r_mdn[qry$cell] <- qry$fmado_cluster_mdn
 eucld_r <- tmp; eucld_r[qry$cell] <- qry$eucld_cluster; plot(eucld_r)
 
-fmado_r <- c(fmado_r_cmp,
-             fmado_r_avg,
-             fmado_r_mcq,
-             fmado_r_wrd,
-             fmado_r_wd2)
+# fmado_r <- c(fmado_r_cmp,
+#              fmado_r_avg,
+#              fmado_r_mcq,
+#              fmado_r_wrd,
+#              fmado_r_wd2)
+# 
+# fmado_r <- terra::rast('//CATALOGUE/WFP_ClimateRiskPr1/agroclimExtremes/agex_results/clusters/test.tif')
+# names(fmado_r) <- c('complete','average','mcq','ward','ward2')
+# 
+# pca_model <- terra::prcomp(x = fmado_r)
+# pca_r <- terra::predict(fmado_r, pca_model)
 
-fmado_r <- terra::rast('//CATALOGUE/WFP_ClimateRiskPr1/agroclimExtremes/agex_results/clusters/test.tif')
-names(fmado_r) <- c('complete','average','mcq','ward','ward2')
-
-pca_model <- terra::prcomp(x = fmado_r)
-pca_r <- terra::predict(fmado_r, pca_model)
-
-terra::writeRaster(x = fmado_r, filename = paste0(root,'/agroclimExtremes/agex_results/clusters/EUR_fmado_clusters.tif'), overwrite = T)
-terra::writeRaster(x = eucld_r, filename = paste0(root,'/agroclimExtremes/agex_results/clusters/EUR_eucld_clusters.tif'), overwrite = T)
-saveRDS(object = cdd_roi_ntrd, file = paste0(root,'/agroclimExtremes/results/ts_processed.rds'))
+terra::writeRaster(x = fmado_r, filename = paste0(root,'/agroclimExtremes/agex_results/clusters/AUS_cdd_fmado_trimmed_clusters.tif'), overwrite = T)
+terra::writeRaster(x = eucld_r, filename = paste0(root,'/agroclimExtremes/agex_results/clusters/AUS_cdd_eucld_clusters.tif'), overwrite = T)
+# saveRDS(object = cdd_roi_ntrd, file = paste0(root,'/agroclimExtremes/results/ts_processed.rds'))
 
 par(mfrow = c(2,5))
 for(i in 1:10){
