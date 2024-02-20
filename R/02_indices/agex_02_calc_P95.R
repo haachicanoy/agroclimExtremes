03c61c7d5698cb615b43b5ecc32abc128bc45fdb
Achicanoy Estrella <h.a.achicanoy@cgiar.org>
2024-02-20 15:34:39 -0500

    script per index

--- R/02_indices/agex_02_calc_P95.R
+++ R/02_indices/agex_02_calc_P95.R
@@ -1,5 +1,5 @@
 ## ------------------------------------------ ##
-## Compute agro-climatic indices
+## Percentile 95th of daily precipitation
 ## By: Harold Achicanoy
 ## WUR & ABC
 ## Dec. 2023
@@ -19,13 +19,6 @@ root <- '//CATALOGUE/WFP_ClimateRiskPr1'
 
 # Precipitation files
 prc <- list.files(path = paste0(root,'/1.Data/AgERA5/precipitation_flux'), pattern = '.nc$', full.names = T)
-# Minimum temperature files
-tmn <- list.files(...)
-# Mean temperature files
-tav <- list.files(...)
-# Maximum temperature files
-tmx <- list.files(...)
-# ...
 
 # ------------------------------------------ #
 # One season
@@ -45,7 +38,6 @@ s1_ini[is.na(s1_dff)] <- NA
 s1_end[is.na(s1_dff)] <- NA
 
 # >>> Indices computation
-# Percentile 95th of daily precipitation
 yrs <- 1979:2023
 lapply(X = yrs, FUN = function(yr){
   prc_flt <- prc[grep(pattern = paste0('_',yr,'[0-9][0-9][0-9][0-9]_'), x = prc)] # Files filtered per year
@@ -53,16 +45,6 @@ lapply(X = yrs, FUN = function(yr){
   rsl <- terra::rapp(x = rnf, first = s1_ini, last = s1_end, fun = p95); gc(T)
   names(rsl) <- c('p95','day')
   terra::writeRaster(x = rsl, filename = paste0(root,'/agroclimExtremes/agex_indices/agex_p95/agex_p95_intermediate/one_s1_y1_p95_',yr,'.tif'), overwrite = T); gc(T)
-  return('Done.\n')
-})
-
-# Maximum number of consecutive dry days
-lapply(X = yrs, FUN = function(yr){
-  prc_flt <- prc[grep(pattern = paste0('_',yr,'[0-9][0-9][0-9][0-9]_'), x = prc)] # Files filtered per year
-  rnf <- terra::rast(prc_flt); gc(T)
-  rsl <- terra::rapp(x = rnf, first = s1_ini, last = s1_end, fun = cdd); gc(T)
-  names(rsl) <- paste0('cdd_',yr)
-  terra::writeRaster(x = rsl, filename = paste0(root,'/agroclimExtremes/agex_indices/agex_cdd/agex_cdd_intermediate/one_s1_y1_cdd_',yr,'.tif'), overwrite = T); gc(T)
   return('Done.\n')
 })
 
@@ -76,7 +58,6 @@ s1_ini[is.na(s1_dff)] <- NA
 s1_end[is.na(s1_dff)] <- NA
 
 # >>> Indices computation
-# Percentile 95th of daily precipitation
 yrs <- 1979:2023
 lapply(X = yrs, FUN = function(yr){
   # Precipitation files filtered
@@ -93,39 +74,13 @@ lapply(X = yrs, FUN = function(yr){
   }
 })
 
-# Maximum number of consecutive dry days
-lapply(X = yrs, FUN = function(yr){
-  # Precipitation files filtered
-  if(yr < 2023){
-    prc_flt <- prc[unlist(grep2(pattern = c(paste0('_',yr,'[0-9][0-9][0-9][0-9]_'),paste0('_',yr+1,'[0-9][0-9][0-9][0-9]_')), x = prc))] # To implement
-    rnf <- terra::rast(prc_flt)
-    dys <- ifelse(lubridate::leap_year(yr),366,365)
-    rsl <- terra::rapp(x = rnf, first = s1_ini, last = s1_end+dys, fun = cdd)
-    terra::writeRaster(x = rsl, filename = paste0(root,'/agroclimExtremes/agex_indices/agex_cdd/agex_cdd_intermediate/one_s1_y2_cdd_',yr,'.tif'), overwrite = T); gc(T)
-    return('Done.\n')
-  } else {
-    break
-    return('Done.\n')
-  }
-})
-
 # >>> Merging results of One-year and Two-years growing seasons
-# Percentile 95th of daily precipitation
 yrs <- 1979:2022
 lapply(X = yrs, FUN = function(yr){
   fls <- list.files(pattern = paste0('_',yr,'.tif'), path = paste0(root,'/agroclimExtremes/agex_indices/agex_p95/agex_p95_intermediate'), full.names = T)
   p95 <- lapply(fls, terra::rast)
   p95 <- terra::merge(x = p95[[1]], y = p95[[2]])
   terra::writeRaster(x = p95, filename = paste0(root,'/agroclimExtremes/agex_indices/agex_p95/agex_p95_10km/one_s1_p95_',yr,'.tif'), overwrite = T)
-})
-
-# Maximum number of consecutive dry days
-yrs <- 1979:2022
-lapply(X = yrs, FUN = function(yr){
-  fls <- list.files(pattern = paste0('_',yr,'.tif'), path = paste0(root,'/agroclimExtremes/agex_indices/agex_cdd/agex_cdd_intermediate'), full.names = T)
-  cdd <- lapply(fls, terra::rast)
-  cdd <- terra::merge(x = cdd[[1]], y = cdd[[2]])
-  terra::writeRaster(x = cdd, filename = paste0(root,'/agroclimExtremes/agex_indices/agex_cdd/agex_cdd_10km/one_s1_cdd_',yr,'.tif'), overwrite = T)
 })
 
 # ------------------------------------------ #
@@ -148,7 +103,6 @@ s1_ini[is.na(s1_dff)] <- NA
 s1_end[is.na(s1_dff)] <- NA
 
 # >>> Indices computation
-# Percentile 95th of daily precipitation
 yrs <- 1979:2023
 lapply(X = yrs, FUN = function(yr){
   prc_flt <- prc[grep(pattern = paste0('_',yr,'[0-9][0-9][0-9][0-9]_'), x = prc)] # Files filtered per year
@@ -156,16 +110,6 @@ lapply(X = yrs, FUN = function(yr){
   rsl <- terra::rapp(x = rnf, first = s1_ini, last = s1_end, fun = p95); gc(T)
   names(rsl) <- c('p95','day')
   terra::writeRaster(x = rsl, filename = paste0(root,'/agroclimExtremes/agex_indices/agex_p95/agex_p95_intermediate/two_s1_y1_p95_',yr,'.tif'), overwrite = T); gc(T)
-  return('Done.\n')
-})
-
-# Maximum number of consecutive dry days
-lapply(X = yrs, FUN = function(yr){
-  prc_flt <- prc[grep(pattern = paste0('_',yr,'[0-9][0-9][0-9][0-9]_'), x = prc)] # Files filtered per year
-  rnf <- terra::rast(prc_flt); gc(T)
-  rsl <- terra::rapp(x = rnf, first = s1_ini, last = s1_end, fun = cdd); gc(T)
-  names(rsl) <- paste0('cdd_',yr)
-  terra::writeRaster(x = rsl, filename = paste0(root,'/agroclimExtremes/agex_indices/agex_cdd/agex_cdd_intermediate/two_s1_y1_cdd_',yr,'.tif'), overwrite = T); gc(T)
   return('Done.\n')
 })
 
@@ -179,7 +123,6 @@ s1_ini[is.na(s1_dff)] <- NA
 s1_end[is.na(s1_dff)] <- NA
 
 # >>> Indices computation
-# Percentile 95th of daily precipitation
 yrs <- 1979:2023
 lapply(X = yrs, FUN = function(yr){
   # Precipitation files filtered
@@ -197,40 +140,13 @@ lapply(X = yrs, FUN = function(yr){
   }
 })
 
-# Maximum number of consecutive dry days
-lapply(X = yrs, FUN = function(yr){
-  # Precipitation files filtered
-  if(yr < 2023){
-    prc_flt <- prc[unlist(grep2(pattern = c(paste0('_',yr,'[0-9][0-9][0-9][0-9]_'),paste0('_',yr+1,'[0-9][0-9][0-9][0-9]_')), x = prc))] # To implement
-    rnf <- terra::rast(prc_flt)
-    dys <- ifelse(lubridate::leap_year(yr),366,365)
-    rsl <- terra::rapp(x = rnf, first = s1_ini, last = s1_end+dys, fun = cdd)
-    names(rsl) <- paste0('cdd_',yr)
-    terra::writeRaster(x = rsl, filename = paste0(root,'/agroclimExtremes/agex_indices/agex_cdd/agex_cdd_intermediate/two_s1_y2_cdd_',yr,'.tif'), overwrite = T); gc(T)
-    return('Done.\n')
-  } else {
-    break
-    return('Done.\n')
-  }
-})
-
 # >>> Merging results of One-year and Two-years growing seasons
-# Percentile 95th of daily precipitation
 yrs <- 1979:2022
 lapply(X = yrs, FUN = function(yr){
   fls <- list.files(pattern = paste0('two_s1_y[0-9]_p95_',yr,'.tif'), path = paste0(root,'/agroclimExtremes/agex_indices/agex_p95/agex_p95_intermediate'), full.names = T)
   p95 <- lapply(fls, terra::rast)
   p95 <- terra::merge(x = p95[[1]], y = p95[[2]])
   terra::writeRaster(x = p95, filename = paste0(root,'/agroclimExtremes/agex_indices/agex_p95/agex_p95_10km/two_s1_p95_',yr,'.tif'), overwrite = T)
-})
-
-# Maximum number of consecutive dry days
-yrs <- 1979:2022
-lapply(X = yrs, FUN = function(yr){
-  fls <- list.files(pattern = paste0('two_s1_y[0-9]_cdd_',yr,'.tif'), path = paste0(root,'/agroclimExtremes/agex_indices/agex_cdd/agex_cdd_intermediate'), full.names = T)
-  cdd <- lapply(fls, terra::rast)
-  cdd <- terra::merge(x = cdd[[1]], y = cdd[[2]])
-  terra::writeRaster(x = cdd, filename = paste0(root,'/agroclimExtremes/agex_indices/agex_cdd/agex_cdd_10km/two_s1_cdd_',yr,'.tif'), overwrite = T)
 })
 
 ## Season 2
@@ -244,7 +160,6 @@ s2_ini[is.na(s2_dff)] <- NA
 s2_end[is.na(s2_dff)] <- NA
 
 # >>> Indices computation
-# Percentile 95th of daily precipitation
 yrs <- 1979:2023
 lapply(X = yrs, FUN = function(yr){
   prc_flt <- prc[grep(pattern = paste0('_',yr,'[0-9][0-9][0-9][0-9]_'), x = prc)] # Files filtered per year
@@ -252,16 +167,6 @@ lapply(X = yrs, FUN = function(yr){
   rsl <- terra::rapp(x = rnf, first = s2_ini, last = s2_end, fun = p95); gc(T)
   names(rsl) <- c('p95','day')
   terra::writeRaster(x = rsl, filename = paste0(root,'/agroclimExtremes/agex_indices/agex_p95/agex_p95_intermediate/two_s2_y1_p95_',yr,'.tif'), overwrite = T); gc(T)
-  return('Done.\n')
-})
-
-# Maximum number of consecutive dry days
-lapply(X = yrs, FUN = function(yr){
-  prc_flt <- prc[grep(pattern = paste0('_',yr,'[0-9][0-9][0-9][0-9]_'), x = prc)] # Files filtered per year
-  rnf <- terra::rast(prc_flt); gc(T)
-  rsl <- terra::rapp(x = rnf, first = s2_ini, last = s2_end, fun = cdd); gc(T)
-  names(rsl) <- paste0('cdd_',yr)
-  terra::writeRaster(x = rsl, filename = paste0(root,'/agroclimExtremes/agex_indices/agex_cdd/agex_cdd_intermediate/two_s2_y1_cdd_',yr,'.tif'), overwrite = T); gc(T)
   return('Done.\n')
 })
 
@@ -275,7 +180,6 @@ s2_ini[is.na(s2_dff)] <- NA
 s2_end[is.na(s2_dff)] <- NA
 
 # >>> Indices computation
-# Percentile 95th of daily precipitation
 yrs <- 1979:2023
 lapply(X = yrs, FUN = function(yr){
   # Precipitation files filtered
@@ -293,38 +197,11 @@ lapply(X = yrs, FUN = function(yr){
   }
 })
 
-# Maximum number of consecutive dry days
-lapply(X = yrs, FUN = function(yr){
-  # Precipitation files filtered
-  if(yr < 2023){
-    prc_flt <- prc[unlist(grep2(pattern = c(paste0('_',yr,'[0-9][0-9][0-9][0-9]_'),paste0('_',yr+1,'[0-9][0-9][0-9][0-9]_')), x = prc))] # To implement
-    rnf <- terra::rast(prc_flt)
-    dys <- ifelse(lubridate::leap_year(yr),366,365)
-    rsl <- terra::rapp(x = rnf, first = s2_ini, last = s2_end+dys, fun = cdd)
-    names(rsl) <- paste0('cdd_',yr)
-    terra::writeRaster(x = rsl, filename = paste0(root,'/agroclimExtremes/agex_indices/agex_cdd/agex_cdd_intermediate/two_s2_y2_cdd_',yr,'.tif'), overwrite = T); gc(T)
-    return('Done.\n')
-  } else {
-    break
-    return('Done.\n')
-  }
-})
-
 # >>> Merging results of One-year and Two-years growing seasons
-# Percentile 95th of daily precipitation
 yrs <- 1979:2022
 lapply(X = yrs, FUN = function(yr){
   fls <- list.files(pattern = paste0('two_s2_y[0-9]_p95_',yr,'.tif'), path = paste0(root,'/agroclimExtremes/agex_indices/agex_p95/agex_p95_intermediate'), full.names = T)
   p95 <- lapply(fls, terra::rast)
   p95 <- terra::merge(x = p95[[1]], y = p95[[2]])
   terra::writeRaster(x = p95, filename = paste0(root,'/agroclimExtremes/agex_indices/agex_p95/agex_p95_10km/two_s2_p95_',yr,'.tif'), overwrite = T)
-})
-
-# Maximum number of consecutive dry days
-yrs <- 1979:2022
-lapply(X = yrs, FUN = function(yr){
-  fls <- list.files(pattern = paste0('two_s2_y[0-9]_cdd_',yr,'.tif'), path = paste0(root,'/agroclimExtremes/agex_indices/agex_cdd/agex_cdd_intermediate'), full.names = T)
-  cdd <- lapply(fls, terra::rast)
-  cdd <- terra::merge(x = cdd[[1]], y = cdd[[2]])
-  terra::writeRaster(x = cdd, filename = paste0(root,'/agroclimExtremes/agex_indices/agex_cdd/agex_cdd_10km/two_s2_cdd_',yr,'.tif'), overwrite = T)
 })
