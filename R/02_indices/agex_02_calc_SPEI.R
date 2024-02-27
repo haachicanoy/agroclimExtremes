545a637f7cbd5dd2118daa4d06360f983bdc7670
Achicanoy Estrella <h.a.achicanoy@cgiar.org>
2024-02-27 10:03:37 -0500

    calc spei

---
+++ R/02_indices/agex_02_calc_SPEI.R
@@ -0,0 +1,39 @@
+# ------------------------------------------ #
+# SPEI
+# By: Harold Achicanoy & Cesar Saavedra
+# WUR & ABC
+# Feb. 2024
+# ------------------------------------------ #
+
+# R options and packages loading
+options(warn = -1, scipen = 999)
+suppressMessages(if(!require(pacman)){install.packages('pacman')}else{library(pacman)})
+suppressMessages(pacman::p_load(terra,SPEI))
+
+# Root directory
+root <- '//CATALOGUE/WFP_ClimateRiskPr1'
+
+# SPEI scale
+scl <- 6
+
+# List water balance files
+blc_fls <- list.files(path = paste0(root,'/agroclimExtremes/agex_raw_data/monthly_balance'), pattern = '.tif$', full.names = T)
+nms_fls <- gsub('.tif','', gsub('bal-', '', basename(blc_fls)))
+
+# Out files
+out_pth <- paste0(root,'/agroclimExtremes/agex_raw_data/monthly_spei')
+out_fls <- paste0(out_pth,'/spei-',scl,'_',nms_fls,'.tif')
+
+# Load water balance files
+blc <- terra::rast(blc_fls); names(blc) <- nms_fls
+
+calc_spei <- function(x){
+  x <- ts(data = x, start = c(1979,1), frequency = 12)
+  Spei <- SPEI::spei(data = x, scale = scl, na.rm = T)$fitted |> as.numeric()
+  return(Spei)
+}
+
+# Calculate SPEI 
+Spei <- terra::app(x = blc, fun = calc_spei)
+names(Spei) <- nms_fls
+terra::writeRaster(Spei, out_fls, overwrite = T)
