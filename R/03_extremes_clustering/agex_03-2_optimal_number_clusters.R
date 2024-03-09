7a44e551ac90b26afcc1fff239fbbe2ce53b594f
Achicanoy Estrella <h.a.achicanoy@cgiar.org>
2024-03-08 20:58:25 -0500

    parallel again

--- R/03_extremes_clustering/agex_03-2_optimal_number_clusters.R
+++ R/03_extremes_clustering/agex_03-2_optimal_number_clusters.R
@@ -19,13 +19,23 @@ fmado_smp_dist <- get_fmado_dist(x_smp); gc(T)   # F-madogram distances
 fmado_smp_dist <- cap_fmado_dist(fmado_smp_dist) # Truncated F-madogram distances
 gc(T)
 
-future::plan(cluster, workers = 30, gc = T)
-sil_width <- furrr::future_map(.x = 2:200, .f = function(k){
-  model <- cluster::pam(x = fmado_smp_dist, k = k)
-  results <- data.frame(k = k, avg_silhouette = model$silinfo$avg.width)
-  return(results)
-}) |> dplyr::bind_rows()
-future:::ClusterRegistry('stop'); gc(T)
+if(OSys == 'Windows'){
+  future::plan(cluster, workers = 30, gc = T)
+  sil_width <- furrr::future_map(.x = 2:200, .f = function(k){
+    model <- cluster::pam(x = fmado_smp_dist, k = k)
+    results <- data.frame(k = k, avg_silhouette = model$silinfo$avg.width)
+    return(results)
+  }) |> dplyr::bind_rows()
+  future:::ClusterRegistry('stop'); gc(T)
+} else {
+  future::plan(multicore, workers = 30, gc = T)
+  sil_width <- future.apply::future_lapply(X = 2:200, FUN = function(k){
+    model <- cluster::pam(x = fmado_smp_dist, k = k)
+    results <- data.frame(k = k, avg_silhouette = model$silinfo$avg.width)
+    return(results)
+  }) |> dplyr::bind_rows()
+  future:::ClusterRegistry('stop'); gc(T)
+}
 
 # Plot the relationship between k and avg_silhouette
 ggplot(sil_width, aes(x = k, y = avg_silhouette)) +
