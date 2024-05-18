f3e633ad9190d9b1947f3169d0a105befd8153d9
Achicanoy Estrella <h.a.achicanoy@cgiar.org>
2024-05-18 02:22:15 -0500

    Paper statistics

---
+++ R/05_figures/agex_05_paper_statistics.R
@@ -0,0 +1,62 @@
+## ------------------------------------------ ##
+## Paper statistics
+## By: Harold Achicanoy
+## WUR & ABC
+## May 2024
+## ------------------------------------------ ##
+
+## R options and packages loading ----
+options(warn = -1, scipen = 999)
+suppressMessages(if(!require(pacman)){install.packages('pacman')}else{library(pacman)})
+suppressMessages(pacman::p_load(terra,MetBrewer,rnaturalearth,tidyverse,ggpubr,tseries,biscale,pals,cowplot,gridExtra,grid))
+
+# Activate Windows fonts
+extrafont::font_import()
+extrafont::loadfonts(device = 'win')
+extrafont::fonts()
+
+## Setup arguments ----
+root   <- '//CATALOGUE/WFP_ClimateRiskPr1'
+index  <- 'spei-6'
+
+## Extreme drought clusters ----
+# Categorical
+agex_sgn <- terra::rast(paste0(root,'/agroclimExtremes/agex_results/agex_results_clusters/agex_global_spei-6_combined_fmadogram_clean.tif'))
+names(agex_sgn) <- 'extreme_cluster'
+cls <- data.frame(extreme_cluster = sort(unique(terra::values(agex_sgn))))
+cls$id <- cls$extreme_cluster
+cls <- cls[,c('id','extreme_cluster')]
+cls$extreme_cluster <- as.character(cls$extreme_cluster)
+levels(agex_sgn) <- cls; rm(cls)
+
+# Numerical
+agex_sgn_num <- terra::rast(paste0(root,'/agroclimExtremes/agex_results/agex_results_clusters/agex_global_spei-6_combined_fmadogram_clean.tif'))
+names(agex_sgn_num) <- 'extreme_cluster'
+
+# Metrics
+agex_sgn_metrics <- utils::read.csv(paste0(root,'/agroclimExtremes/agex_results/agex_all_metrics.csv'))
+
+## Clusters presence ----
+cat('Extreme drought clusters in one country (%):',
+    round(table(agex_sgn_metrics$countries_count)[[1]]/sum(table(agex_sgn_metrics$countries_count)) * 100, 1),
+    '\n')
+cat('Extreme drought clusters in two or more countries (%):',
+    100 - round(table(agex_sgn_metrics$countries_count)[[1]]/sum(table(agex_sgn_metrics$countries_count)) * 100, 1),
+    '\n')
+
+## One growing season
+cat('One-GS. Extreme drought clusters in one country (%):',
+    round(table(agex_sgn_metrics$countries_count[agex_sgn_metrics$growing_seasons == 1])[[1]]/sum(table(agex_sgn_metrics$countries_count[agex_sgn_metrics$growing_seasons == 1])) * 100, 1),
+    '\n')
+# Extreme drought clusters in more than one country (%)
+cat('One-GS. Extreme drought clusters in two or more countries (%):',
+    100 - round(table(agex_sgn_metrics$countries_count[agex_sgn_metrics$growing_seasons == 1])[[1]]/sum(table(agex_sgn_metrics$countries_count[agex_sgn_metrics$growing_seasons == 1])) * 100, 1),
+    '\n')
+
+## Two growing seasons
+cat('Two-GS. Extreme drought clusters in one country (%):',
+    round(table(agex_sgn_metrics$countries_count[agex_sgn_metrics$growing_seasons == 2])[[1]]/sum(table(agex_sgn_metrics$countries_count[agex_sgn_metrics$growing_seasons == 2])) * 100, 1),
+    '\n')
+cat('Two-GS. Extreme drought clusters in two or more countries (%):',
+    100 - round(table(agex_sgn_metrics$countries_count[agex_sgn_metrics$growing_seasons == 2])[[1]]/sum(table(agex_sgn_metrics$countries_count[agex_sgn_metrics$growing_seasons == 2])) * 100, 2),
+    '\n')
