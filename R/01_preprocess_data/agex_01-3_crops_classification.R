cbdb0a6401511f5c648cce92bbc5f401a4f41dbb
Achicanoy Estrella <h.a.achicanoy@cgiar.org>
2026-01-30 15:13:31 -0500

    Documentation of Crop Classification Count script

--- R/01_preprocess_data/agex_01-3_crops_classification.R
+++ R/01_preprocess_data/agex_01-3_crops_classification.R
@@ -1,18 +1,21 @@
-# ------------------------------------------ #
-# Crops classification - croplands
+# --------------------------------------------------------------- #
+# Global hotspots of co-occurring extreme droughts in agriculture
+# Crop classification counts at 0.1° on agricultural production areas
 # By: Harold Achicanoy
 # WUR & ABC
-# Feb. 2024
-# ------------------------------------------ #
+# Created in February 2024
+# Modified in January 2026
+# --------------------------------------------------------------- #
 
-# R options and packages loading
+# R options and user-defined functions
 options(warn = -1, scipen = 999)
+suppressMessages(if(!require(pacman)){install.packages('pacman')}else{library(pacman)})
+suppressMessages(pacman::p_load(terra))
 list.files2 <- Vectorize(FUN = list.files, vectorize.args = 'pattern')
 
 # Define directories
-root <- '//CATALOGUE/AgroclimExtremes'             # Server
-inp_dir <- 'D:/Data/Maps'                          # Local
-out_dir <- 'D:/OneDrive - CGIAR/PhD/papers/paper1' # local
+root <- '//CATALOGUE/AgroclimExtremes'
+inp_dir <- paste0(root,'/agex_raw_data/croplands')
 
 # Load MapSPAM readme
 mapspam_info <- readLines(paste0(root,'/agex_raw_data/croplands/spam2010/ReadMe_v2r0_Global.txt'))
@@ -63,7 +66,7 @@ grps <- unique(agr$GROUP)
 
 # Count the number of crops per crop class
 for(i in 1:length(grps)){
-  if(!file.exists(paste0(out_dir,'/data/agex_',gsub(' ','_',grps[i]),'_count.tif'))){
+  if(!file.exists(paste0(root,'/agex_raw_data/agex_',gsub(' ','_',grps[i]),'_count.tif'))){
     # List files per group
     crops_fls <- list.files2(path = crops_dir,
                              pattern = paste0(toupper(agr$`SPAM name`[agr$GROUP == grps[i]]),'_A.tif'),
@@ -71,7 +74,7 @@ for(i in 1:length(grps)){
     crops_lnd <- crops_fls |> purrr::map(.f = function(x){
       r <- terra::rast(x); r[r != 0] <- 1; return(r)
     }) |> terra::rast() |> sum()
-    terra::writeRaster(x = crops_lnd, filename = paste0(out_dir,'/data/agex_',gsub(' ','_',grps[i]),'_count.tif'), overwrite = T)
+    terra::writeRaster(x = crops_lnd, filename = paste0(root,'/agex_raw_data/agex_',gsub(' ','_',grps[i]),'_count.tif'), overwrite = T)
     if(!file.exists(paste0(root,'/agex_raw_data/agex_',gsub(' ','_',grps[i]),'_count_10km.tif'))){
       # Resampling MapSPAM into AgERA5 template resolution
       crops_lnd_10km <- terra::resample(x = crops_lnd, y = tmp_10km, method = 'near')
