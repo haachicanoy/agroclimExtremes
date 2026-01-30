4d3c002e0c5ef9da813eeaf3ac215dfce00171dc
Achicanoy Estrella <h.a.achicanoy@cgiar.org>
2026-01-30 14:56:00 -0500

    Documentation of Prepare Growing Season Data script

--- R/01_preprocess_data/agex_01-2_prepare_growing_seasons.R
+++ R/01_preprocess_data/agex_01-2_prepare_growing_seasons.R
@@ -1,11 +1,13 @@
-# ------------------------------------------ #
-# Prepare growing seasons - croplands
+# --------------------------------------------------------------- #
+# Global hotspots of co-occurring extreme droughts in agriculture
+# Prepare growing seasons on agricultural production areas
 # By: Harold Achicanoy
 # WUR & ABC
-# Dec. 2023
-# ------------------------------------------ #
+# Created in December 2023
+# Modified in January 2026
+# --------------------------------------------------------------- #
 
-# R options and packages loading
+# R options and user-defined functions
 options(warn = -1, scipen = 999)
 suppressMessages(if(!require(pacman)){install.packages('pacman')}else{library(pacman)})
 suppressMessages(pacman::p_load(terra,fields,spatstat,future,furrr))
@@ -19,14 +21,11 @@ root <- '//CATALOGUE/AgroclimExtremes'
 aux  <- paste0(root,'/agex_raw_data/agex_phenology')
 out  <- paste0(root,'/agex_raw_data'); dir.create(out, F, T)
 
-# Mapping dekads to day-of-year units
+# --------------------------------------------------------------- #
+# Dekads to day-of-year units
+# --------------------------------------------------------------- #
 yrs <- 1979:2023
 doy <- lapply(X = yrs, FUN = function(yr){
-  ## Select the median date within 10-days window
-  # dys <- seq(from = as.Date(paste0(yr,'-01-01')), to = as.Date(paste0(yr,'-12-31')), by = 'day')
-  # dks <- split(dys, ceiling(seq_along(dys)/10))
-  # dks <- dks[1:36]
-  # dks <- purrr::map(.x = dks, .f = function(x){as.character(median(x))}) |> unlist() |> as.Date()
   ## Select the initial date within 10-days window
   dks <- seq(from = as.Date(paste0(yr,'-01-01')), to = as.Date(paste0(yr,'-12-31')), length.out = 36)
   doy <- data.frame(dkd = lubridate::yday(dks))
@@ -36,26 +35,38 @@ doy <- apply(X = doy, MARGIN = 1, FUN = function(x){round(mean(x))})
 dks_map <- data.frame(day = doy, dekad = 1:36); rm(yrs, doy)
 dks_map$day[dks_map$dekad == 36] <- 1
 
-# Load number of growing seasons per site
+# --------------------------------------------------------------- #
+# Number of growing seasons per site at 0.25°
+# --------------------------------------------------------------- #
 if(!file.exists(paste0(out,'/agex_nseasons_25km_corrected.tif'))){
+  
+  # Number of growing seasons at ~1 km
   n_seasons <- terra::rast(paste0(root,'/agex_raw_data/agex_phenology/phenonseasons_v03.tif')); gc(T)
   n_seasons <- terra::resample(x = n_seasons, y = tmp_25km, method = 'near', threads = T); gc(T) # Growing seasons per site at 25 km resolution
   croplands <- terra::rast(paste0(root,'/agex_raw_data/agex_croplands_foods_mapspam_25km.tif'))
+  # Mask number-of-growing-season raster by agricultural production areas
   n_seasons <- terra::mask(x = n_seasons, mask = croplands)
   terra::writeRaster(x = n_seasons, filename = paste0(out,'/agex_nseasons_25km.tif'), overwrite = T)
+  # The previous raster includes misclassified pixels. This means that regions
+  # with one growing season include pixels with two growing seasons, and
+  # vice versa, due to the resampling step.
+  
+  # To overcome this issue, we performed a spatial density analysis to identify
+  # the threshold from which we can classify pixels as one or two growing
+  # seasons accordingly.
   
   # Split seasons
-  n_seasons_1 <- n_seasons # Season 1
+  n_seasons_1 <- n_seasons # One season
   n_seasons_1[n_seasons_1 != 1] <- NA
   n_seasons_1_dfm <- terra::as.data.frame(x = n_seasons_1, xy = T, cell = T, na.rm = T) # Coordinates
   
-  n_seasons_2 <- n_seasons # Season 2
+  n_seasons_2 <- n_seasons # Two seasons
   n_seasons_2[n_seasons_2 != 2] <- NA
   n_seasons_2_dfm <- terra::as.data.frame(x = n_seasons_2, xy = T, cell = T, na.rm = T)
   
   r_owin <- as.owin(unlist(as.list(terra::ext(n_seasons))))
   
-  # Get probabilities from Season 1 coordinates
+  # Get densities (probabilities) from 'One season' coordinates
   p1 <- ppp(x = n_seasons_1_dfm$x, y = n_seasons_1_dfm$y, window = r_owin)
   ds1 <- density(p1)
   ds_r1 <- terra::rast(ds1)
@@ -63,7 +74,7 @@ if(!file.exists(paste0(out,'/agex_nseasons_25km_corrected.tif'))){
   ds_r1 <- terra::mask(x = ds_r1, mask = n_seasons_1)
   ds_r1 <- ds_r1/sum(terra::values(ds_r1, na.rm = T))
   
-  # Get probabilities from Season 2 coordinates
+  # Get densities from 'Two seasons' coordinates
   p2 <- ppp(x = n_seasons_2_dfm$x, y = n_seasons_2_dfm$y, window = r_owin)
   ds2 <- density(p2)
   ds_r2 <- terra::rast(ds2)
@@ -85,12 +96,9 @@ if(!file.exists(paste0(out,'/agex_nseasons_25km_corrected.tif'))){
   rm(probs, ds_r1, ds_r2, ds1, ds2, p1, p2, r_owin, n_seasons_1_dfm, n_seasons_2_dfm, n_seasons_1, n_seasons_2)
   
   # Visualize outliers
-  hist(n_seasons_dfm$probability[n_seasons_dfm$nseasons == 2])
-  thr <- 0.000017
-  abline(v = thr, col = 2, lty = 2)
-  plot(n_seasons)
-  points(n_seasons_dfm[n_seasons_dfm$nseasons == 2 & n_seasons_dfm$probability < thr,c('x','y')],
-         col = 'red', pch = 20)
+  thr <- 0.000017 # Obtained after several iterations
+  hist(n_seasons_dfm$probability[n_seasons_dfm$nseasons == 2]); abline(v = thr, col = 2, lty = 2)
+  plot(n_seasons); points(n_seasons_dfm[n_seasons_dfm$nseasons == 2 & n_seasons_dfm$probability < thr,c('x','y')], col = 'red', pch = 20)
   
   n_seasons_dfm2 <- n_seasons_dfm
   n_seasons_dfm2$nseasons[n_seasons_dfm2$nseasons == 2 & n_seasons_dfm2$probability < thr] <- 1
@@ -102,27 +110,16 @@ if(!file.exists(paste0(out,'/agex_nseasons_25km_corrected.tif'))){
   terra::writeRaster(x = aux, filename = paste0(out,'/agex_nseasons_25km_corrected.tif'), overwrite = T)
   saveRDS(object = n_seasons_dfm, file = paste0(out,'/agex_nseasons_25km_dfm.rds'))
   rm(n_seasons_dfm2, n_seasons_dfm, aux, croplands, thr)
-  
-  # hist(n_seasons_dfm$probability[n_seasons_dfm$nseasons == 1])
-  # thr <- 0.0000011
-  # abline(v = thr, col = 2, lty = 2)
-  # plot(-1*(n_seasons-2))
-  # points(n_seasons_dfm[n_seasons_dfm$nseasons == 1 & n_seasons_dfm$probability < thr,c('x','y')],
-  #        col = 'red', pch = 20)
 } else {
   n_seasons <- terra::rast(paste0(out,'/agex_nseasons_25km_corrected.tif'))
   n_seasons_dfm <- readRDS(file = paste0(out,'/agex_nseasons_25km_dfm.rds'))
   thr <- 0.000017
 }
 
-# clls <- n_seasons_dfm$cell[n_seasons_dfm$nseasons == 2 & n_seasons_dfm$probability < thr]
-# 
-# adj_clls <- terra::adjacent(x = n_seasons, cells = clls, directions = 'queen', pairs = F, symmetrical = T)
-# terra::extract(x = n_seasons, y = terra::xyFromCell(n_seasons, cell = adj_clls[6,]))
-
-# ------------------------------------------ #
+# --------------------------------------------------------------- #
+# Starting and ending dates of growing season
 # One season - Season 1 pre-processing
-# ------------------------------------------ #
+# --------------------------------------------------------------- #
 # Start date
 if(!file.exists(paste0(aux,'/agex_one_s1_ini_raw_25km.tif'))){
   s1_ini <- terra::rast(paste0(aux,'/phenos1_v03.tif'))
@@ -161,9 +158,10 @@ s1_end_25km <- terra::subst(s1_end_25km, from = dks_map$dekad, to = dks_map$day)
 terra::writeRaster(s1_end_25km, filename = paste0(out,'/agex_one_s1_end_25km.tif'), overwrite = T)
 rm(s1_end_25km); gc(T)
 
-# ------------------------------------------ #
+# --------------------------------------------------------------- #
+# Starting and ending dates of growing season
 # Two seasons - Season 1 pre-processing
-# ------------------------------------------ #
+# --------------------------------------------------------------- #
 # Start date
 if(!file.exists(paste0(aux,'/agex_two_s1_ini_raw_25km.tif'))){
   s1_ini <- terra::rast(paste0(aux,'/phenos1_v03.tif'))
@@ -202,9 +200,10 @@ s1_end_25km <- terra::subst(s1_end_25km, from = dks_map$dekad, to = dks_map$day)
 terra::writeRaster(s1_end_25km, filename = paste0(out,'/agex_two_s1_end_25km.tif'), overwrite = T)
 rm(s1_end_25km); gc(T)
 
-# ------------------------------------------ #
+# --------------------------------------------------------------- #
+# Starting and ending dates of growing season
 # Two seasons - Season 2 pre-processing
-# ------------------------------------------ #
+# --------------------------------------------------------------- #
 # Start date
 if(!file.exists(paste0(aux,'/agex_two_s2_ini_raw_25km.tif'))){
   s2_ini <- terra::rast(paste0(aux,'/phenos2_v03.tif'))
@@ -243,10 +242,9 @@ s2_end_25km <- terra::subst(s2_end_25km, from = dks_map$dekad, to = dks_map$day)
 terra::writeRaster(s2_end_25km, filename = paste0(out,'/agex_two_s2_end_25km.tif'), overwrite = T)
 rm(s2_end_25km); gc(T)
 
-## ------------------------------------------ ##
-## Filtering of growing seasons within croplands
-## ------------------------------------------ ##
-
+# --------------------------------------------------------------- #
+# Mask growing-season dates by agricultural production areas
+# --------------------------------------------------------------- #
 # One season
 s1_ini_25km <- terra::rast(paste0(out,'/agex_one_s1_ini_25km.tif'))
 croplnds    <- terra::rast(paste0(out,'/agex_croplands_foods_mapspam_25km.tif'))
